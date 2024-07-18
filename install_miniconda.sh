74e1e2b8879b3c3b58703f0d59f714a8edcc410a
yanghoeg <yjw.cpa@google.com>
2024-07-18 14:00:49 +0900

    miniconda add

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 varname=$(basename $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/home/*)
 
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 apt update
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-armv7l.sh
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 chmod +x ~/Miniconda3-latest-Linux-armv7l.sh
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 bash ~/Miniconda3-latest-Linux-armv7l.sh
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 source ~/.bashrc
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 conda update conda
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 rm ~/Miniconda3-latest-Linux-armv7l.sh
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt update
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-armv7l.sh
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 chmod +x ./Miniconda3-latest-Linux-armv7l.sh
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 bash ./Miniconda3-latest-Linux-armv7l.sh
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 source ~/.bashrc
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 conda update conda
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 rm ~/Miniconda3-latest-Linux-armv7l.sh
