d1cd0847492c81553e575e42f694f708005ce986
yanghoeg <yjw.cpa@google.com>
2024-07-22 22:40:27 +0900

    dbeaver, thunderbird

--- install_thunderbird.sh
+++ install_thunderbird.sh
@@ -2,8 +2,8 @@
 varname=$(basename $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/home/*)
 
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 apt update
-proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 wget https://thunderbird.io/files/thunderbird-ce-latest-linux.gtk.aarch64-nojdk.tar.gz -O thunderbird.tar.gz
-proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 tar -xvzf ./thunderbird.tar.gz
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/dbeaver/dbeaver/releases/download/24.1.3/dbeaver-ce-24.1.3-linux.gtk.aarch64-nojdk.tar.gz -O thunderbird.tar.gz
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 tar -xvzf thunderbird.tar.gz
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 mv thunderbird /opt/
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo ln -s /opt/thunderbird/thunderbird /usr/bin/thunderbird
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install apt-utils openjdk-11-jdk -y
