51f4aff6ce4d8574d0ec038a6a4cad59bb3d7d0a
yanghoeg <gojilla6@naver.com>
2024-07-23 19:01:34 +0900

    thunderbird

--- install_thunderbird.sh
+++ install_thunderbird.sh
@@ -2,9 +2,10 @@
 
 varname=$(basename $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/home/*)
 
-proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 echo "deb [signed-by="/usr/share/keyrings/ubuntu-archive-keyring.gpg"] http://ports.ubuntu.com/ubuntu-ports mantic main universe multiverse" >> $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/etc/apt/source.list 
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sed -i 's/noble/mantic/g' $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/etc/apt/source.list 
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt update
-proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install thunderbird
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install thunderbird -y
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sed -i 's/mantic/noble/g' $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/etc/apt/source.list 
 
 cp $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/usr/share/applications/thunderbird.desktop $PREFIX/share/applications
 sed -i "s/^Exec=\(.*\)$/Exec=proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 \1/"   $PREFIX/share/applications/thunderbird.desktop
