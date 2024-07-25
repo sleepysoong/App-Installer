43e60d32e6e09c86b12f42a011ea7822f6058c38
yanghoeg <gojilla6@naver.com>
2024-07-25 17:30:06 +0900

    add sasm

--- install_sasm.sh
+++ install_sasm.sh
@@ -5,7 +5,6 @@ proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sed -
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt update
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install sasm
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 echo "alias sasm='QT_SCALE_FACTOR=2 sasm'" >> ~/.bashrc
-proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 source ~/.bashrc
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 curl -LO https://answers.launchpad.net/ubuntu/+source/sasm/3.14.0+ds-1/+build/26331457/+files/sasm-dbgsym_3.14.0+ds-1_arm64.ddeb
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo dpkg -i sasm-dbgsym_3.14.0+ds-1_arm64.ddeb
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 rm sasm-dbgsym_3.14.0+ds-1_arm64.ddeb
