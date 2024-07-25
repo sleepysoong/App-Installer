e0698a5ef26c0d402512a3f677c7ba181f563dc7
yanghoeg <gojilla6@naver.com>
2024-07-25 21:42:46 +0900

    add sasm

--- install_sasm.sh
+++ install_sasm.sh
@@ -4,7 +4,7 @@ varname=$(basename $HOME/../usr/var/lib/proot-distro/installed-rootfs/ubuntu/hom
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sed -i 's/noble/mantic/' $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/etc/apt/sources.list 
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt update
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo apt install sasm
-proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 echo "alias sasm='QT_SCALE_FACTOR=2 sasm'" >> $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/home/$varname/.bashrc
+proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 bash -c "echo \"alias sasm='QT_SCALE_FACTOR=2 sasm'\" >> $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/home/$varname/.bashrc"
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 curl -LO https://answers.launchpad.net/ubuntu/+source/sasm/3.14.0+ds-1/+build/26331457/+files/sasm-dbgsym_3.14.0+ds-1_arm64.ddeb
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 sudo dpkg -i sasm-dbgsym_3.14.0+ds-1_arm64.ddeb
 proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 rm sasm-dbgsym_3.14.0+ds-1_arm64.ddeb
@@ -15,7 +15,7 @@ Type=Application
 Name=sasm
 GenericName=sasm
 Comment=sasm
-Exec=proot-distro login ubuntu --user $varname --shared-tmp -- env DISPLAY=:1.0 QT_SCALE_FACTOR=2 sasm
+Exec=prun QT_SCALE_FACTOR=2 sasm
 Categories=Development;
 Icon=naver-whale
 Path=
