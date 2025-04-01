ade79e29117b57e9d30b485cdec41645168b4595
yanghoeg <134801173+yanghoeg@users.noreply.github.com>
2025-04-01 17:26:16 +0900

    Update install_vscode.sh

--- install_vscode.sh
+++ install_vscode.sh
@@ -2,7 +2,7 @@
 varname=$(basename $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/home/*)
 
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 apt update
-proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 wget https://packages.microsoft.com/repos/code/pool/main/c/code/code_1.82.0-1694038208_arm64.deb -O code.deb
+proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 wget https://packages.microsoft.com/repos/code/pool/main/c/code/code_1.98.2-1741787947_arm64.deb -O code.deb
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install ./code.deb -y
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 rm code.deb
 proot-distro login ubuntu --shared-tmp -- env DISPLAY=:1.0 sudo -S apt install gpg software-properties-common apt-transport-https -y
