61cb71bbab8b3a3e8e65aad6d14d0c5b3076d31c
yanghoeg <134801173+yanghoeg@users.noreply.github.com>
2025-04-01 19:36:37 +0900

    Update install_vscode.sh

--- install_vscode.sh
+++ install_vscode.sh
@@ -3,7 +3,7 @@ varname=$(basename $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/home/*)
 
 prun apt update
 prun wget https://packages.microsoft.com/repos/code/pool/main/c/code/code_1.98.2-1741787947_arm64.deb -O code.deb
-prun sudo -S dpkg -i ./code.deb -y
+prun sudo -S dpkg -i ./code.deb
 prun rm code.deb
 prun sudo -S apt install gpg software-properties-common apt-transport-https -y
 prun wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | prun sudo apt-key add -
